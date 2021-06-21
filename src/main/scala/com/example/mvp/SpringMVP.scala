41f2ef544821bd251159b621d27a6213da302cb0
augustnagro <augustnagro@gmail.com>
2021-06-20 19:57:11 -0700

    Tomcat/start working now; implemented SpringBootServletInitializer

--- src/main/scala/com/example/mvp/SpringMVP.scala
+++ src/main/scala/com/example/mvp/SpringMVP.scala
@@ -4,10 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.SpringApplication
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.boot.builder.SpringApplicationBuilder
 
 @SpringBootApplication
 @RestController
-class SpringMVP:
+class SpringMVP extends SpringBootServletInitializer:
+
+  override def configure(app: SpringApplicationBuilder): SpringApplicationBuilder =
+    app.sources(classOf[SpringMVP])
 
   @GetMapping
   def userName: String = "Gus"
